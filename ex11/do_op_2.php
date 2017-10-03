5a8f85821aaaead24d0aa11c6397eec94f674a93
Timothee HUGO <thugo@e2r10p22.42.fr>
2017-10-03 17:11:47 +0200

    ex11

---
+++ ex11/do_op_2.php
@@ -0,0 +1,42 @@
+#!/usr/bin/php
+<?php
+	if ($argc == 2) {
+		$argv[1] = trim($argv[1]);
+		$tab = preg_split('/(\+|\-|\*|\/|\%)/', $argv[1], -1, PREG_SPLIT_DELIM_CAPTURE);
+		
+		if (count($tab) == 3) {
+			$tab[0] = trim($tab[0]);
+			$tab[1] = trim($tab[1]);
+			$tab[2] = trim($tab[2]);
+			
+			if (is_numeric($tab[0]) && is_numeric($tab[2])) {
+				switch($tab[1]) {
+					case '+':
+						echo ($tab[0] + $tab[2])."\n";
+						break;
+					case '-':
+						echo ($tab[0] - $tab[2])."\n";
+						break;
+					case '*':
+						echo ($tab[0] * $tab[2])."\n";
+						break;
+					case '/':
+						echo ($tab[0] / $tab[2])."\n";
+						break;
+					case '%':
+						echo ($tab[0] % $tab[2])."\n";
+						break;
+					default:
+						echo "Syntax Error3\n";
+				}
+			}
+			else
+				echo "Syntax Error2\n";
+		}
+		else 
+			echo "Syntax Error1\n";
+	}
+	else {
+		echo "Incorrect Parameters\n";
+	}
+?>
